f62534e554b5163e019731eefc216bcdca51e7b9
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-24 08:45:08 +0300

    DZ7_3

---
+++ less7/DZ3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 6, 4, 0, 15, 1 }; // Произвольный массив
+
+        PrintArrayReversed(array, array.Length - 1); // Вызов рекурсивной функции для печати элементов массива
+
+        Console.ReadLine();
+    }
+
+    static void PrintArrayReversed(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(array[index]); // Вывод текущего элемента
+
+            PrintArrayReversed(array, index - 1); // Рекурсивный вызов для следующего элемента
+        }
+    }
+}
